ed59e81225e235e9be9b88fec457b5bf0d2a0bb2
Melvin Salas <melvinsalas10@gmail.com>
2016-04-03 18:47:26 +0100

    Update language

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-  res.send('About Us');
+  res.send('About Us!');
 });
 
 //console.log(__dirname);
